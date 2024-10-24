4922a5187ac995d462a90872c9ccbab9b3b21a97
Balanced0 <167624337+Balanced0@users.noreply.github.com>
2024-10-24 12:30:51 +0600

    4A_Watermelon

--- 4A_Watermelon.java
+++ 4A_Watermelon.java
@@ -10,7 +10,7 @@ public class Watermelon\{\
     public static void main(String[] args)\{\
         Scanner sc = new Scanner(System.in);\
         int w = sc.nextInt();\
-        if(w%2 == 0)\{\
+        if(w > 2 && w%2 == 0)\{\
             System.out.println("YES");\
         \}\
         else\{\
